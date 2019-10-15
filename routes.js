64c6753f03b5dfc4f7ce766dab8d8700d6bac49a
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-10-14 21:30:48 -0400

    adding routes

---
+++ routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express(); // factory function
+
+/*
+function express(){
+
+
+ return obj
+}
+
+*/
+
+const conn = new Sequelize();
+
+
+/*
+function Sequelize(){
+ //....
+}
+
+*/
